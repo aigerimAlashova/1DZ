08f4059eb97af6003d96e5737265b41cc36c5e70
aigerimAlashova <aigerimalasheva39@gmail.com>
2023-01-09 15:39:49 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// let i = 0;
+// const printArrayValues = ()=> {
+//    i++;
+//    if(i >3){
+//  return "array"
+//    } 
+//    console.log(i , "array");
+//    printArrayValues()
+
+//    console.log(arr[i],'array');
+// }
+// printArrayValues()
+
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6,7,8,9,10];
+// let arr3 = arr1.concat(arr2);
+
+// console.log(arr3);
+
+
+// let arr = [1,2,3,4,5];
+// for (let i = 0; i<arr.length; i++);
+// console.log(arr.pop())
+
+
+const str = "'loremipsumdolor','o'"
+
+console.log(str.slice(3).match(/o/g));
+ console.log("'loremipsumdolor,'o'".match(/o/g));
